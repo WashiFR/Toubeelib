1f9e8ef088d9d93454e8f5988dff97d628be7c33
Weier Loris <lorisweier@gmx.fr>
2024-10-08 17:10:43 +0200

    Middleware

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -2,7 +2,9 @@
 
 use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
-
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use toubeelib\application\middlewares\CorsMiddleware;
 
 $builder = new ContainerBuilder();
 $builder->addDefinitions(__DIR__ . '/settings.php' );
@@ -11,6 +13,12 @@ $builder->addDefinitions(__DIR__ . '/dependencies.php');
 $c=$builder->build();
 $app = AppFactory::createFromContainer($c);
 
+// Middleware
+$app->add(new CorsMiddleware());
+$app->options('/{routes:.+}',
+    function(Request $rq, Response $rs, array $args) : Response {
+        return $rs;
+    });
 
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
